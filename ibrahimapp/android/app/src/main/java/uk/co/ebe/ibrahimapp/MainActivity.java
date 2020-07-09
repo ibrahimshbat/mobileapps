ae1e480c5b21bac4b1ebde998d62196a5fa116c2
Ibrahim Elsanosi <ibrahimsabat@yahoo.com>
2020-07-09 14:42:45 +0100

    first commit

---
+++ ibrahimapp/android/app/src/main/java/uk/co/ebe/ibrahimapp/MainActivity.java
@@ -0,0 +1,6 @@
+package uk.co.ebe.ibrahimapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ibrahimapp/lib/pages/Resume.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class Resume extends StatefulWidget {
+  @override
+  _ResumeState createState() => _ResumeState();
+}
+
+class _ResumeState extends State<Resume> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(
+        "My CV",
+      ),
+    );
+  }
+}
