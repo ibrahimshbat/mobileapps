d7832083781755e0c6f73bbbd85fb503bed5546f
Ibrahim Elsanosi <ibrahimsabat@yahoo.com>
2020-07-22 23:47:25 +0100

    Add padding

--- weatherapp/lib/pages/Home.dart
+++ weatherapp/lib/pages/Home.dart
@@ -41,11 +41,18 @@ class _HomeState extends State<Home> {
         title: Text('Weather Forecast'),
         centerTitle: true,
       ),
-      body: Center(
-          child:Text(
-            intweather.toString(),
-            textAlign: TextAlign.center,
-          ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 200.0, 0, 0),
+        child: Column(
+          children: <Widget>[
+            Center(
+                child:Text(
+                  intweather.toString(),
+                  textAlign: TextAlign.center,
+                ),
+            ),
+          ],
+        ),
       ),
       );
   }
